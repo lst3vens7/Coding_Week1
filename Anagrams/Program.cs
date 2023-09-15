2905519fbee94006f5d456ddee2341aabeabc46e
lst3vens7 <145001217+lst3vens7@users.noreply.github.com>
2023-09-15 09:50:30 +0100

    Add project files.

---
+++ Anagrams/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagrams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "parts", "traps", "arts", "rats", "starts", "tarts", "rat", "art", "tar", "tars", "stars", "stray" };
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length == 4 & (words[i].Contains("s")) & (words[i].Contains("t")) & (words[i].Contains("a")) & (words[i].Contains("r")))
+                {
+                }
+                else
+                {
+                    words[i] = string.Empty;
+                }
+            }
+
+            Console.Write("Anagrams for star are: ");
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] != string.Empty)
+                {
+                    Console.Write(words[i] + " ");
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Coding_Week1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Week1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
